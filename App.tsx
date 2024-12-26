0e134bb9e3a3969ec0719540fd68ea764e532d3f
wesleyeduardodev <wesleyeduardo.dev@gmail.com>
2024-12-26 11:25:32 -0300

    Utilizando StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,27 +1,33 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
     return (
-        <View style={{
-            flex: 1,
-            backgroundColor: '#131016',
-            padding: 24
-        }}>
-            <Text style={{
-                color: '#FDFCFE',
-                fontSize: 24,
-                fontWeight: 'bold',
-                marginTop: 48
-            }}>
-                Nome do evento
+        <View style={styles.container}>
+            <Text style={styles.eventName}>
+               Wesley Eduardo
             </Text>
 
-            <Text style={{
-                color: '#6B6B6B',
-                fontSize: 16
-            }}>
-                Sexta, 4 de Novembro de 2022.
+            <Text style={styles.eventDate}>
+                Quinta, 26 de Dezembro de 2024.
             </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24
+    },
+    eventName: {
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+    },
+    eventDate: {
+        color: '#6B6B6B',
+        fontSize: 16
+    }
+})
