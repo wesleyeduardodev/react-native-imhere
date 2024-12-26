e2c8e54ae9c218c9b9ae09ea80f367ef0c13ca60
wesleyeduardodev <wesleyeduardo.dev@gmail.com>
2024-12-26 10:47:47 -0300

    Criando exemplo inicial

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+      <View>
+        <Text key="1">React</Text>
+        <Text key="2">Native</Text>
+      </View>
+  )
+}
