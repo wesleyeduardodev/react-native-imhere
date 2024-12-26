80aa31182d6f8d7deb3da51a4b8d601dd3ef98e5
wesleyeduardodev <wesleyeduardo.dev@gmail.com>
2024-12-26 12:14:48 -0300

    Botão de Adicionar

--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
@@ -23,5 +23,17 @@ export const styles = StyleSheet.create({
         color: '#FFFF',
         padding: 16,
         fontSize: 16,
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 24
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#31CF67',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
